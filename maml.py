3a61aa1cf1d345c76d27f0cf276a37864fa487e2
qwerhacks <arjun.subramonian@gmail.com>
2020-03-03 20:33:57 -0800

    Made updates

--- maml.py
+++ maml.py
@@ -17,28 +17,36 @@ from torch.utils.data import Dataset
 
 
 class Net(nn.Module):
-    def __init__(self):
+    def __init__(self, roberta):
         super(Net, self).__init__()
-        self.roberta = torch.hub.load('pytorch/fairseq', 'roberta.large').model
+        self.roberta = roberta.model
         self.fc1 = nn.Linear(1024, 512)
         self.fc2 = nn.Linear(512, 1)
 
-    def forward(self, x):
-        tokens = self.roberta.encode(x)
-        if tokens.dim() == 1:
-            tokens = tokens.unsqueeze(0)
-        if tokens.size(-1) > self.roberta.max_positions():
-            raise ValueError('tokens exceeds maximum length: {} > {}'.format(
-                tokens.size(-1), self.roberta.max_positions()
-            ))
-        x, extra = self.roberta(
-            tokens,
-            features_only=True,
-            return_all_hiddens=False,
-        )
+    def forward(self, tokens_list):
+        sentence_embeddings = []
+        for tokens in tokens_list:
+            if tokens.dim() == 1:
+                tokens = tokens.unsqueeze(0)
+            if tokens.size(-1) > self.roberta.max_positions():
+                raise ValueError('tokens exceeds maximum length: {} > {}'.format(
+                    tokens.size(-1), self.roberta.max_positions()
+                ))
+            x, extra = self.roberta(
+                tokens,
+                features_only=True,
+                return_all_hiddens=True,
+            )
+            inner_states = extra['inner_states']
+            pooling_layer = inner_states[-2].transpose(0, 1)
+            sentence_embeddings.append(pooling_layer.mean(1).view(1024))
+        
+        x = torch.stack(sentence_embeddings)
+
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
-        return F.sigmoid(x)
+        x = F.sigmoid(x)
+        return torch.squeeze(x)
 
 def accuracy(predictions, targets):
     predictions = predictions > 0.5
@@ -52,19 +60,14 @@ class TestDataset(Dataset):
     def __init__(self, text_dataset, transform=None):
         self.text_dataset = text_dataset
         self.transform = transform
-        # self.roberta = torch.hub.load('pytorch/fairseq', 'roberta.large')
-        # print(type(self.roberta.model))
 
     def __len__(self):
         return len(self.text_dataset)
 
     def __getitem__(self, idx):
-        
-        sample = (' '.join(getattr(self.text_dataset[idx], 'text')), int(getattr(self.text_dataset[idx], 'label') == 'positive'))
-        # label = sample[1]
-        # data_tokens = self.roberta.encode(sample[0])
-        # data = self.roberta.extract_features(data_tokens)
-        # sample = (data, label)
+        tokens = getattr(self.text_dataset[idx], 'text')
+        labels = int(getattr(self.text_dataset[idx], 'label') == 'positive')
+        sample = (tokens, labels)
         # if self.transform:
         #     sample = self.transform(sample)
         return sample
@@ -74,7 +77,9 @@ class TestDataset(Dataset):
 def main(lr=0.005, maml_lr=0.01, iterations=1000, ways=2, shots=1, tps=32, fas=5, device=torch.device("cpu"),
          download_location='~/data'):
 
-    x = torchtext.datasets.SST("/Users/arjuns/Downloads/trees/test.txt", torchtext.data.Field(lower=False), torchtext.data.Field(sequential=False))
+    roberta = torch.hub.load('pytorch/fairseq', 'roberta.large')
+
+    x = torchtext.datasets.SST("/Users/arjuns/Downloads/trees/test.txt", torchtext.data.Field(sequential=False), torchtext.data.Field(sequential=False))
     # transformations = transforms.Compose([
     #     transforms.ToTensor()
     # ])
@@ -83,20 +88,19 @@ def main(lr=0.005, maml_lr=0.01, iterations=1000, ways=2, shots=1, tps=32, fas=5
 
     train_tasks = l2l.data.TaskDataset(train,
                                        task_transforms=[
-                                            l2l.data.transforms.NWays(train, n=ways),
+                                            l2l.data.transforms.NWays(train, ways),
                                             l2l.data.transforms.KShots(train, 2*shots),
                                             l2l.data.transforms.LoadData(train),
-                                            # l2l.data.transforms.RemapLabels(train),
-                                            # l2l.data.transforms.ConsecutiveLabels(train),
+                                            l2l.data.transforms.RemapLabels(train),
+                                            l2l.data.transforms.ConsecutiveLabels(train),
                                        ],
                                        num_tasks=10)
-    print(train_tasks.sample_task_description())
 
-    model = Net()
+    model = Net(roberta)
     # model.to(device)
     meta_model = l2l.algorithms.MAML(model, lr=maml_lr)
     opt = optim.Adam(meta_model.parameters(), lr=lr)
-    loss_func = nn.NLLLoss(reduction='mean')
+    loss_func = nn.CrossEntropyLoss()
 
     for iteration in range(iterations):
         iteration_error = 0.0
@@ -106,18 +110,28 @@ def main(lr=0.005, maml_lr=0.01, iterations=1000, ways=2, shots=1, tps=32, fas=5
             train_task = train_tasks.sample()
             data, labels = train_task
             # data = data.to(device)
-            labels = labels.to(device)
+            # labels = labels.to(device)
 
             # Separate data into adaptation/evalutation sets
             adaptation_indices = np.zeros(len(data), dtype=bool)
             adaptation_indices[np.arange(shots*ways) * 2] = True
-            evaluation_indices = torch.from_numpy(~adaptation_indices)
-            adaptation_indices = torch.from_numpy(adaptation_indices)
+            evaluation_indices = ~adaptation_indices
+            adaptation_indices = adaptation_indices
+            data = np.array(data)
+            labels = np.array(labels)
+
             adaptation_data, adaptation_labels = data[adaptation_indices], labels[adaptation_indices]
+            adaptation_data = [roberta.encode(elem) for elem in adaptation_data]
+            # adaptation_labels = torch.Tensor(adaptation_labels).view(len(adaptation_labels), 1)
+
             evaluation_data, evaluation_labels = data[evaluation_indices], labels[evaluation_indices]
+            evaluation_data = [roberta.encode(elem) for elem in evaluation_data]
+            # evaluation_labels = torch.Tensor(evaluation_labels).view(len(evaluation_labels), 1)
 
             # Fast Adaptation
             for step in range(fas):
+                print(learner(adaptation_data).shape)
+                print(adaptation_labels.shape)
                 train_error = loss_func(learner(adaptation_data), adaptation_labels)
                 learner.adapt(train_error)
 
@@ -140,10 +154,10 @@ def main(lr=0.005, maml_lr=0.01, iterations=1000, ways=2, shots=1, tps=32, fas=5
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Learn2Learn MNIST Example')
+    parser = argparse.ArgumentParser(description='Learn2Learn SST Example')
 
-    parser.add_argument('--ways', type=int, default=5, metavar='N',
-                        help='number of ways (default: 5)')
+    parser.add_argument('--ways', type=int, default=2, metavar='N',
+                        help='number of ways (default: 2)')
     parser.add_argument('--shots', type=int, default=1, metavar='N',
                         help='number of shots (default: 1)')
     parser.add_argument('-tps', '--tasks-per-step', type=int, default=32, metavar='N',
